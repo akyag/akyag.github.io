dfd21fbfc68ca1947d6642ce759bca29036562ff
Akshay Ghate <akshay.226@gmail.com>
2022-10-11 19:19:52 +0530

    dark mode working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [preact(), tailwind()],
+	vite: {
+		ssr: {
+			external: ["svgo"],
+		},
+	},
 });
