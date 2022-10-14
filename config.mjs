9802663480df659b5413b74df540039982a49337
Akshay Ghate <akshay.226@gmail.com>
2022-10-14 17:42:46 +0530

    modular designe done, most pages are done

---
+++ config.mjs
@@ -0,0 +1,9 @@
+export const SITE = {
+    name: 'Web-o-Reviews',
+
+    origin: 'https://weboreviews.com',
+    basePathname: '/',
+
+    title: 'Weboreviews - Engaging, purposeful, and creative.',
+    description: 'Pune based responsive website design services.'
+}
