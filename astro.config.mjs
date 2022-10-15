d90d8b7472a3f647eeffeccb9503323ac5d93749
Akshay Ghate <akshay.226@gmail.com>
2022-10-15 18:45:05 +0530

    404 page added, meta tags tinkered

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://weboreviews.com',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), compress()]
 });
