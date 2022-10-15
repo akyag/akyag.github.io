b28dc41d0e3ade38cad216f6219fe6114775d0c6
Akshay Ghate <akshay.226@gmail.com>
2022-10-15 19:14:00 +0530

    robots generator, data path changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
-
+import robotsTxt from 'astro-robots-txt';
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://weboreviews.com',
-  integrations: [tailwind(), sitemap(), compress()]
+  integrations: [tailwind(), sitemap(), robotsTxt(), compress()]
 });
