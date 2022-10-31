81c110d9f25f45bb42e77e72d81adca0b3649d9b
Akshay Ghate <akshay.226@gmail.com>
2022-10-31 16:57:19 +0530

    Added prefetch and lowered modal bg brightness.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from 'astro-robots-txt';
 import compress from "astro-compress";
-import "@fontsource/montserrat"
-import "@fontsource/mulish"
+import "@fontsource/montserrat";
+import "@fontsource/mulish";
 
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://weboreviews.com',
   trailingSlash: 'always',
-  integrations: [tailwind(), sitemap(), robotsTxt(), compress()]
+  integrations: [tailwind(), sitemap(), robotsTxt(), compress(), prefetch()]
 });
