f2c36a0130e1d93de4b23e7fc5d4da4b1dea755c
Akshay Ghate <akshay.226@gmail.com>
2022-12-17 09:43:19 +0530

    Chore: package updates, back to SVGs again.

--- astro.config.ts
+++ astro.config.ts
@@ -1,27 +1,36 @@
-import { defineConfig } from 'astro/config'
-import UnoCSS from 'unocss/astro'
+import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
 
 // Plugins
 import sitemap from "@astrojs/sitemap";
-import robotsTxt from 'astro-robots-txt';
+import robotsTxt from "astro-robots-txt";
 import compress from "astro-compress";
 import prefetch from "@astrojs/prefetch";
 import image from "@astrojs/image";
-
 
 // Fonts
 import "@fontsource/montserrat";
 import "@fontsource/mulish";
 
 // Styles
-import "@unocss/reset/tailwind.css"
-import "./src/styles/global.css"
+import "@unocss/reset/tailwind.css";
+import "./src/styles/global.css";
 
 export default defineConfig({
-  site: 'https://weboreviews.com',
-  trailingSlash: 'always',
+  site: "https://weboreviews.com",
+  trailingSlash: "always",
   integrations: [
-    UnoCSS(), sitemap(), robotsTxt(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), compress(), prefetch()]
+    UnoCSS(),
+    sitemap(),
+    robotsTxt(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    compress(),
+    prefetch(),
+  ],
+  experimental: {
+    prerender: true,
+    errorOverlay: true,
+  },
 });
