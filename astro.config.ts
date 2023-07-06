1d07f26bf0715c023ba709e63417242f0d7f299b
Akshay Ghate <akshay.226@gmail.com>
2023-07-06 13:04:50 +0530

    Feat: Adopting experimental astro assets feature.

--- astro.config.ts
+++ astro.config.ts
@@ -6,16 +6,6 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import compress from "astro-compress";
 import prefetch from "@astrojs/prefetch";
-import image from "@astrojs/image";
-
-// Fonts
-// import "@fontsource/montserrat";
-// import "@fontsource/mulish";
-// import "@fontsource/atkinson-hyperlegible";
-
-// Styles
-import "@unocss/reset/tailwind-compat.css";
-import "./src/styles/global.css";
 
 export default defineConfig({
   site: "https://weboreviews.com/",
@@ -25,11 +15,11 @@ export default defineConfig({
     }),
     sitemap(),
     robotsTxt(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     prefetch(),
     compress(),
   ],
   compressHTML: true,
+  experimental: {
+    assets: true,
+  },
 });
